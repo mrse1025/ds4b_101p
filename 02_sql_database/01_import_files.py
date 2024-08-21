dda58c81a81675e04e38ee405482cf52cc148b8e
Victoria Espinola <victoriaespinola@Victorias-MacBook-Pro.local>
2024-08-20 20:46:09 -0500

    first commit.

---
+++ 02_sql_database/01_import_files.py
@@ -0,0 +1,23 @@
+# DS4B 101-P: PYTHON FOR BUSINESS ANALYSIS ----
+# Module 2 (Pandas Import): Importing Files ----
+
+# IMPORTS ----
+#%%
+import pandas as pd
+
+#%%
+
+# 1.0 FILES ----
+
+# - Pickle ----
+pickle_df = pd.read_pickle("./00_data_wrangled/bike_orderlines_wrangled_df.pkl")
+pickle_df.info()
+# - CSV ----
+csv_df = pd.read_csv("./00_data_wrangled/bike_orderlines_wrangled_df.csv",
+            parse_dates = ['order_date'])
+csv_df.info()
+
+# - Excel ----
+excel_df = pd.read_excel("./00_data_wrangled/bike_orderlines_wrangled_df.xlsx")
+excel_df.info()
+# %%
