87261a7dfe0890e30536486667c74b213d0dc66c
Victoria Espinola <victoriaespinola@MacBook-Pro.attlocal.net>
2024-08-24 09:14:08 -0500

    creating function with for loop

--- 02_sql_database/03_func_collect_data.py
+++ 02_sql_database/03_func_collect_data.py
@@ -2,6 +2,29 @@
 # SQL DATABASES (Module 2): Working with SQLAlchemy ----
 
 # IMPORTS ----
+import sqlalchemy as sql
+import pandas as pd
 
 # FUNCTION DEFINITION ----
+def collect_data(conn_string = "sqlite:///00_database/bike_orders_database.sqlite"):
+    #Body
+    
+    #1.0 Connect to Database
+    engine = sql.create_engine(conn_string)
+    
+    conn = engine.connect()
+    
+    #accessing the data
+    table_names = ['bikes', 'bikeshops', 'orderlines']
+    
+    data_dict = {}
+    for table in table_names: 
+        data_dict[table] = pd.read_sql(f"SELECT * FROM {table}", con = conn)
+    
+    conn.close()
+    
+    #2.0 Combining & cleaning data
+    
+    return data_dict
 
+collect_data()
