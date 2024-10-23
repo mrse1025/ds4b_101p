9a17ab49eda61e930b62596810e991f8f70d85cf
mrse1025@gmail.com <mrse1025@gmail.com>
2024-10-22 20:27:01 -0500

    Getting started with Dates: date math, date sequences

--- 04_time_series/02_time_series.py
+++ 04_time_series/02_time_series.py
@@ -7,32 +7,58 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from my_pandas_extensions.database import collect_data
+from my_panda_extensions.database import collect_data
 
 # DATA
 
 df = collect_data()
 
 # 1.0 DATE BASICS
-
-
+df['order_date']
 
 # Conversion
+type("2011-01-07")
+
+pd.to_datetime("2011-01-07").to_period(freq = "W").to_timestamp() #converts str to date, creates a period, converts to time stamp
 
 
 # Accessing elements
 
 # Months
+df.order_date #time stamp data
 
-
+df.order_date.dt.month #returns applicable months
+df.order_date.dt.month_name() 
 # Days
+df.order_date.dt.day #returns numeric days in data set
 
+#Year
+df.order_date.dt.year
 
 # DATE MATH
+import datetime
+today = datetime.date.today()
+
+pd.to_datetime(today + pd.Timedelta(" 1 day"))
+
+df.order_date + pd.Timedelta(365, "d")
+
+#Duration Time Deltas
+today = datetime.date.today()
+one_year_from_today = today + pd.Timedelta(365, "d")
+
+(one_year_from_today - today) / pd.Timedelta(30, "d")
+pd.Timedelta(one_year_from_today - today) / np.timedelta64(1, "M")
 
 
 # DATE SEQUENCES
-
+#shows number of days given a specific frequency or start and end date with frequency.
+pd.date_range(
+    start = pd.to_datetime("2011-01"), 
+    #periods = 10,
+    end = pd.to_datetime("2011-12-31"),
+    freq = "1W"
+)
 
 
 # PERIODS
