2a8ab5bd4dcf12e3ad127051fe5404fed4fb71bc
mrse1025@gmail.com <mrse1025@gmail.com>
2024-11-29 10:07:49 -0600

    forecasting loop  with results stored in dictionary

--- 06_sktime/01_sktime_forecast.py
+++ 06_sktime/01_sktime_forecast.py
@@ -17,6 +17,9 @@ import pmdarima as pm #additional package needed to get arima to work
 #Visualization plot_series(), *: means can add as many args as needed
 from sktime.utils.plotting import plot_series
 
+#for progress bars
+from tqdm import tqdm
+
 # 1.0 DATA SUMMARIZATIONS ----
 bike_sales_m_df = df \
     .summarize_by_time(
@@ -58,8 +61,6 @@ lower = predictions_ci[['lower']]
 upper = predictions_ci[['upper']]
 
 
-
-
 plot_series(y,
             predictions,
             lower,
@@ -67,5 +68,49 @@ plot_series(y,
 )
 
 # 3.0 MULTIPLE TIME SERIES FORCAST (LOOP) ----
+bike_sales_cat2_m_df.head()
+
+df = bike_sales_cat2_m_df
+
+df[df.columns[0]] #pulls out a column at a time, set up for loop
+
+#Storing the results in a dictionary
+model_results_dict = {}
+for col in tqdm(df.columns):
+    #Series extraction
+    y = df[col]
+    
+    #Modeling
+    forecaster = AutoARIMA(
+        sp = 12, 
+        suppress_warnings= True 
+    )
+    forecaster.fit(y)
+    h = 12
+    
+    #Prediction and Confidence Interval
+    predictions = forecaster.predict(fh = np.arange(1, h+1))
+    predictions_conf_int = forecaster.predict_interval(
+        fh =np.arange(1, h+1),
+        coverage = 0.95
+    )
+    predictions
+    predictions_conf_int
+    
+    #Combine into dataframe
+    ret= pd.concat([y,predictions, predictions_conf_int], axis = 0)
+    ret.column = ["value", "prediction", "ci_lo", "ci_hi"]
+    
+    #update dictionary
+    model_results_dict[col] = ret
+
+model_results_dict    
+model_result_df = pd.concat(model_results_dict) 
+    
+#visualization
+model_results_dict[list(model_results_dict.keys())[2]].plot() 
+   
+
+
 
 
